26aa00233531766b3c0c36142e0b9cd78172658f
gabrielfernandessilva <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-05-15 22:16:04 -0300

    ruby slower than python

---
+++ getprime.rb
@@ -0,0 +1,35 @@
+def prime(num)
+    if num < 2
+        return false
+    end
+    if num == 2
+        return true
+    end
+    if num%2 == 0
+        return false
+    end
+    for i in (3..num-1)
+        if num%i == 0
+            return false
+        end
+    end
+    return true
+end
+
+def yieldprime(target)
+    count = 1
+    primes = 2
+    while true
+        if count == target
+            return primes
+        end
+        primes += 1
+        if prime(primes)
+            count += 1
+        end
+    end
+end
+
+start = Time.now
+puts(yieldprime(Integer(ARGV[0])))
+puts('Time taked: ' + String(Time.now-start))
