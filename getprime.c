b9397bf938e898707e09f3fa1108334c698a95a2
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-08-22 13:59:36 -0300

    gcc == g++

--- getprime.c
+++ getprime.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-int prime(int num) {
+
+int prime(int num)
+{
     if(num < 2)
         return 0;
     if(num == 2)
@@ -17,28 +19,28 @@ int prime(int num) {
     return 1;
 }
 
-int yieldprime(int target) {
-    int count = 1;
-    int primes = 2;
+int yieldprime(int target)
+{
+    int count = 1, primes = 2;
     while(1) {
-        if(count == target)
+        if (count == target)
             return primes;
         primes++;
-        if(prime(primes))
+        if (prime(primes))
             count++;
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     if (argv[1]) {
         clock_t start = clock();
         int result = yieldprime(atoi(argv[1]));
         clock_t stop = clock();
-        double elapsed = (double)(stop - start) / 1000000;
+        double elapsed = (double) (stop-start) / 1000000;
 
         printf("The nth prime number is %d\n", result);
         printf("Taked about %f seconds\n", elapsed);
-
         return 0;
     }
     else {
