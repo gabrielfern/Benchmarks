b16129182ea97d59683e702e6ae77ad5c6acc242
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-09-07 21:12:57 -0300

    node 8.4

--- getprime.js
+++ getprime.js
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
+// tested with node v8.4.0
+
+
+module.exports = {
+    'prime': prime,
+    'yieldprime': yieldprime
+}
 
 
 function prime(num) {
-    if(num < 2)
+    if (num < 2)
         return false
-    if(num == 2)
+    if (num == 2)
         return true
-    if(num%2 == 0)
+    if (num%2 == 0)
         return false
 
-    for(let i = 3; i<Math.floor(num/2); i += 2) {
-        if(num%i == 0)
+    for (let i = 3; i<Math.floor(num/2); i += 2) {
+        if (num%i == 0)
             return false
     }
 
@@ -19,18 +26,24 @@ function prime(num) {
 
 
 function yieldprime(target) {
-    var count = 1
-    var primes = 2
-    while(true) {
-        if(count == target)
+    let count = 1,
+        primes = 2
+    while (true) {
+        if (count == target)
             return primes
         primes++
-        if(prime(primes))
+        if (prime(primes))
             count++
     }
 }
 
 
-console.time('taken time')
-console.log(yieldprime(parseInt(process.argv[2])))
-console.timeEnd('taken time')
+if (require.main) {
+    if (process.argv.length > 2) {
+        console.time('taken time')
+        console.log(yieldprime(parseInt(process.argv[2])))
+        console.timeEnd('taken time')
+    }
+    else
+        console.log('Needed a positive integer as command line argument')
+}
