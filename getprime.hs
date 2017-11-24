fd66e578a25ec4cce771935eaa16d33001a67559
gabrielfernandesufcg <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-11-24 17:54:30 -0300

    haskell

---
+++ getprime.hs
@@ -0,0 +1,25 @@
+prime :: Int -> Bool
+yieldprime :: Int -> Int
+_prime :: Int -> Int -> Bool
+_yieldprime :: Int -> Int -> Int -> Int
+
+prime num = _prime num 2
+
+_prime num 2
+    | num < 2 = False
+    | num == 2 = True
+    | mod num 2 == 0 = False
+    | otherwise = _prime num 3
+
+_prime num count
+    | rest == 0 = False
+    | div num 2 < count && rest /= 0 = True
+    | otherwise = _prime num (count + 2)
+    where rest = mod num count
+
+yieldprime target = _yieldprime target 1 2
+
+_yieldprime target count primes
+    | count == target = primes
+    | prime (primes + 1) == True = _yieldprime target (count + 1) (primes + 1)
+    | otherwise = _yieldprime target count (primes + 1)
