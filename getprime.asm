1d5045d760b3ec576306becbadcf01bebb2550cf
Gabriel Fernandes <gabrielfernndss@gmail.com>
2018-04-03 02:56:54 -0300

    cleaning use of stack to preserve registers

--- getprime.asm
+++ getprime.asm
@@ -1,21 +1,4 @@
 %macro printVal 1
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rdi
-    push rsi
-    push rbp
-    push rsp
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-
     mov rax, %1
   %%printRAX:
     mov rcx, digitSpace
@@ -61,23 +44,6 @@
     mov rsi, nl
     mov rdx, 1
     syscall
-
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rsp
-    pop rbp
-    pop rsi
-    pop rdi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
 %endmacro
 
 
@@ -184,7 +150,6 @@ _start:
 
 
 section .data
-    hit db "hit"
     nl db 10
 
 section .bss
